b224f28cd7699e5c5464b18ad541179c7f45f946
aq-UI-no <tiffanyaquino116@gmail.com>
2024-05-21 16:05:18 -0700

    back to the original script

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,2 +1,2 @@
--- Query to get the structure of the table 'first_table' in the given database
-SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA FROM information_schema.columns WHERE table_name = 'first_table' AND table_schema = DATABASE();
+-- Script that prints full description of table
+DESC first_table;

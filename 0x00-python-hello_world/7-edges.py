b6b38f087f12b3fa27008dbff42906e7491828b8
aq-UI-no <tiffanyaquino116@gmail.com>
2025-02-18 17:10:04 +0300

    removed extra line

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -5,5 +5,4 @@ word_last_2 = "on"
 middle_word = "olberto"
 print(f"First 3 letters: {word_first_3}")
 print(f"Last 2 letters: {word_last_2}")
-print(f"Middle word: {middle_word}")
-
+print(f"Middle word: {middle_word}")
